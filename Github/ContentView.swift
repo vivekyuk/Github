1b25184f76de3faa8f1bfe856e8cebe9e2a0a4d1
Yukti <yuktivivek2007@gmail.com>
2023-06-12 10:38:48 -0500

    Initial Commit

---
+++ Github/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Github
+//
+//  Created by Yukti Vivek on 6/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Github/GithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubApp.swift
+//  Github
+//
+//  Created by Yukti Vivek on 6/12/23.
+//
+
+import SwiftUI
+
+@main
+struct GithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
